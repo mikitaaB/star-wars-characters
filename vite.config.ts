356b7ed7cd377e9e6246bd7870f2acfc8116a960
Mikita <nikita.492@yandex.by>
2023-05-13 15:15:27 +0300

    Fix base in vite.config.ts according to mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	base: "https://mikitaab.github.io/star-wars-characters/",
-	plugins: [react(), eslint()],
+export default defineConfig(({ mode }) => {
+	return {
+		base: mode === "production" ? "/star-wars-characters/" : "/",
+		plugins: [react(), eslint()],
+	};
 });
