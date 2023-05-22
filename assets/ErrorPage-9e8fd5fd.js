871ae3d878201fcb970e6100999a9a3ce2004d72
Mikita <nikita.492@yandex.by>
2023-05-22 04:46:24 +0300

    Updates

---
+++ assets/ErrorPage-9e8fd5fd.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-a8a69a3c.js";const e=()=>r.jsx("div",{children:r.jsx("h1",{children:"An error occured. Page is not found."})});export{e as default};
--- assets/ErrorPage-e2901b13.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-bf622685.js";const e=()=>r.jsx("div",{children:r.jsx("h1",{children:"An error occured. Page is not found."})});export{e as default};
